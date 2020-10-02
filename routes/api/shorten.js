98a094a8735963be6f27d77fdbe85ca2aa9add01
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 15:55:17 +0530

    Saving Url in fb

--- routes/api/shorten.js
+++ routes/api/shorten.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const uniqid = require('uniqid');
 
+const URL = require('../../models/Urls');
+
 //Route GET /api/test/shorten
 router.get('/test',(req, res) => res.json({msg: 'API is Working'}));
 
@@ -11,6 +13,30 @@ router.post('/', (req,res)=>{
         urlData = req.body.url
     }
     console.log('URL is: ', urlData);
+    URL.findOne({url: urlData}, (err, doc) => {
+        if(doc) {
+            console.log('Entry Found in Database.');
+        }
+        else {
+            console.log('This is a Unique URL');
+            const webaddress = new URL({
+                _id: uniqid(),
+                url: urlData,
+
+            });
+            webaddress.save((err) => {
+                if(err) {
+                    console.log(err);
+                }
+                res.send({
+                    url: urlData,
+                    hash: webaddress._id,
+                    status: 200,
+                    statusTxt: 'OK'
+                })
+            })
+        }
+    });
 });
 
 module.exports = router;
