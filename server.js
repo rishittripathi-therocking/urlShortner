03bae21807c9af92ad87bb72243c76f4d7326fd8
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 14:37:40 +0530

    Server updated

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express =require('express');
-const { process } = require('uniqid');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//DATABASE KEy
+
+const db = require('./config/keys').mongoURI;
+
+//connect Mongo Db
+mongoose.connect(db)
+    .then(() => console.log('MongoDb connencted.'))
+    .catch(err=> console.log(err));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server is running at port ${port} `));
