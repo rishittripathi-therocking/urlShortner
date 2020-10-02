e791e5be87476efc283e3ead0af45c1b1a852829
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 16:04:00 +0530

    Redirecting

---
+++ routes/api/redirect.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.router();
+
+const URL = require('../../models/Urls');
+
+//Route GET /api/test/shorten
+router.get('/test',(req, res) => res.json({msg: 'API is Working'}));
+
+//@route GET /api/redirect
+
+router.get('/', (req, res) => {
+    const hash = req.headers.hash;
+
+    URL.findOne({_id: hash})
+        .then((doc) =>{
+            return res.json({ url: doc.url});
+        })
+        .catch((err) =>{
+            return res.status(400).json({error: 'Sorry, This Link May have Expired'});
+        })
+})
+
+module.exports = router;
