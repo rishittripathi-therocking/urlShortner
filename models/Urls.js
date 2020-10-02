deeafadb8296fcc6b8b0865d1fb579097b3a389a
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 14:44:27 +0530

    Schema Model

---
+++ models/Urls.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UrlSchema = new Schema({
+    _id:  {
+        type: String
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    hash: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = URL = mongoose.model('URL',UrlSchema);
