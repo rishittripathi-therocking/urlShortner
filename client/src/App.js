5095e95cad98125329453d375b2d2d8a102d0ff8
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 17:04:15 +0530

    Sending url to api and getting Response

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
@@ -7,17 +8,39 @@ class App extends React.Component {
   state = {
     url: '',
     link: ''
+  };
+  handleChange = (e) => {
+    this.setState(({
+      url: e.target.value
+    }))
   }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/shorten', {
+      url: this.state.url
+    })
+    .then( res => {
+      console.log(res);
+    })
+  };
   render() {
     return (
       <div className="App">
-         <fieldset>
-           <input type="text" name="url" placeholder="Enter url using the http(s) protocol" />
-           <input type="submit" value="shorten" />
-         </fieldset>
-         <fieldset>
-            <span id="result"> {this.state.link}</span>
-         </fieldset>
+        <form onSubmit={this.handleSubmit}>
+            <fieldset>
+              <input 
+                type="text" 
+                name="url" 
+                placeholder="Enter url using the http(s) protocol" 
+                onChange={this.handleChange}
+                />
+              <input type="submit" value="shorten" />
+            </fieldset>
+            <fieldset>
+                <span id="result"> {this.state.link}</span>
+          </fieldset>
+        </form>
+         
       </div>
     );
   }
